49c2b261de84da1c1b1cd741d8a7d93018eaafb0
GHOSTnew <kilien.1997@hotmail.fr>
2012-12-15 18:32:53 +0100

    MàJ

--- BOT-IRC.py
+++ BOT-IRC.py
@@ -5,6 +5,7 @@ import irclib
 import ircbot
 import socket
 import time
+import random
 
 class BotIRCbyGHOST(ircbot.SingleServerIRCBot):
     def __init__(self):
@@ -71,6 +72,13 @@ class BotIRCbyGHOST(ircbot.SingleServerIRCBot):
             serv.privmsg(canal, "\00304\002Vous n'avez pas les privilège sur ce chan\002")
         elif message[0:4] == "&say":
             serv.privmsg(canal, message[5:400])
+        elif message == "&random" :
+            serv.privmsg(canal, "\00304\002Voici un nombre : " + str(random.random()) + "\002")
+        elif message == "&copy" :
+            serv.privmsg(canal, "Team Mondial Production 2012")
+            serv.privmsg(canal, "by GHOSTnew")
+            serv.privmsg(canal, "avec la participation de lumir")
+            serv.privmsg(canal, "source: https://github.com/GHOSTnew/OwnYourIRCBot")
         elif message == "&help":
             serv.privmsg(canal, "\002\037les commandes sont:\037\002")
             serv.privmsg(canal, "\00310\002&join\002 #chan \00315,01(pour que le bot se connecte a un chan)")
@@ -80,7 +88,9 @@ class BotIRCbyGHOST(ircbot.SingleServerIRCBot):
             serv.privmsg(canal, "\00310\002&check\002 \00315,01(verifie si un site est down ou pas)")
             serv.privmsg(canal, "\00310\002&time\002 \00315,01(affiche la date et l'heure)")
             serv.privmsg(canal, "\00310\002&kick\002 \00315,01(kick un joueur)")
+            serv.privmsg(canal, "\00310\002&random\002 \00315,01(génère un nombre aléatoire entre 0 et 1)")
             serv.privmsg(canal, "\00310\002&say\002 \00315,01(permet de faire dire quelque chose au bot)")
+            serv.privmsg(canal, "\00310\002&copy\002 \00315,01(affiche les credits)")
             serv.privmsg(canal, "\00310\002&quit\002 \00315,01(pour déconnecter le bot)")
         elif not canal_verification[1].is_voiced(auteur)and not canal_verification[1].is_halfoper(auteur) and not canal_verification[1].is_oper(auteur):
             for insulte in self.insultes:
